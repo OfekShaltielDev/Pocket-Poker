23dabf6f0b69fe0f900268b42e39fe14ed00644d
Ofek Shaltiel <72337187+OfekShaltielDev@users.noreply.github.com>
2025-07-14 16:01:19 -0500

    Update Login.js

--- pocket-poker/src/pages/Login.js
+++ pocket-poker/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { createClient } from "@supabase/supabase-js"
-const supabase = createClient("https://hplnxayyijxebyklpoiz.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwbG54YXl5aWp4ZWJ5a2xwb2l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk0MTQyMjcsImV4cCI6MjAyNDk5MDIyN30.8XJMoHKCQp_dWVaK1p73HtUQlGWWEz2hu-nAi7gNr8s");
+const supabase = createClient("key 1", "key 2");
 const Login = (props) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
